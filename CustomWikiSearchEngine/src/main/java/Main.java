5a53d57e6b6f9b44f398c55e370e47bc59efb8c5
Romain <romainmichelucci@protonmail.com>
2020-12-26 15:12:58 +0100

    Indexing the documents
    The csv is parsed using OpenCSV and the documents are indexed #11

---
+++ CustomWikiSearchEngine/src/main/java/Main.java
@@ -0,0 +1,134 @@
+import com.opencsv.CSVReader;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
+
+    /**
+     * Parse CSV file in order to get row by row values
+     * @param fileName : csv file name
+     * @return List<List<String>> : list of list of data
+     * @throws Exception csvReader
+     */
+    private List<List<String>> parseCsv(String fileName) throws Exception {
+
+        List<List<String>> records = new ArrayList<>();
+        try (CSVReader csvReader = new CSVReader(new FileReader("src/main/resources/"+fileName))) {
+            String[] values;
+            while ((values = csvReader.readNext()) != null ) {
+                //Be aware of blank lines
+                if (values.length > 1) {
+                    records.add(Arrays.asList(values));
+                }
+            }
+        }
+        //Remove the header
+        records.remove(records.get(0));
+        return records;
+    }
+
+    /**
+     * Create Lucene Index of the wiki documents extracted
+     * @param data all the data extracted from the csv file
+     * @throws Exception indexWriter
+     */
+    private void createIndex(List<List<String>> data) throws Exception {
+
+        Directory dir = FSDirectory.open(Paths.get("index_folder"));
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+
+        iwc.setOpenMode(OpenMode.CREATE); 			// Create a NEW index in the directory
+
+        IndexWriter writer = new IndexWriter(dir, iwc);
+        addDocuments(data,writer);
+
+        writer.close();
+    }
+
+
+    private void addDocuments(List<List<String>> data, IndexWriter writer) throws Exception {
+        int documentsAdded = 0;
+
+        for (List<String> datum : data) {
+            Document doc = getDocument(datum);
+            writer.addDocument(doc);
+            documentsAdded++;
+        }
+
+        System.out.println("documents added: " + documentsAdded);
+    }
+
+
+    private Document getDocument(List<String> value) {
+
+        Document lucene_doc = new Document();
+        lucene_doc.add(new StringField("title", value.get(0), Field.Store.YES));
+        lucene_doc.add(new StringField("abstract", value.get(1), Field.Store.YES));
+        lucene_doc.add(new TextField("content", value.get(2), Field.Store.YES));
+        List<String> listTopics = getTopics(value.get(3));
+        for (String topic : listTopics) {
+            //Lucene documents support the addition of multiple fields of the same name
+            lucene_doc.add(new StringField("topics", topic, Field.Store.YES));
+        }
+        //maybe the link of the article ?
+
+        return lucene_doc;
+
+    }
+
+    private static List<String> getTopics(String topics){
+        String[] d = topics.split("'");//"'|,|\\[|\\]");
+        List<String> listTopics = new ArrayList<>();
+        for (String s : d) {
+            if (s.length() > 2) listTopics.add(s);
+        }
+        return listTopics;
+    }
+
+    /**
+     * Show the best results of a query
+     * @param hits 
+     * @param searcher
+     * @throws IOException
+     */
+    private void showResults(ScoreDoc[] hits, IndexSearcher searcher) throws IOException {
+        for (ScoreDoc scoreDoc : hits) {
+            System.out.println("doc="+scoreDoc.doc+" score="+scoreDoc.score);
+            Document doc = searcher.doc(scoreDoc.doc);
+            System.out.println("\t" + doc.get("title"));
+
+        }
+
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        List<List<String>> documents = main.parseCsv("WikiData.csv");
+
+        main.createIndex(documents);
+
+    }
+
+}
