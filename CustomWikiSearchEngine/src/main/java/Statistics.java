b9685353b53ecf973e60c52244a278d51582441d
Romain <romainmichelucci@protonmail.com>
2021-01-05 17:16:24 +0100

    Some general statistics functions
    #19

--- CustomWikiSearchEngine/src/main/java/Statistics.java
+++ CustomWikiSearchEngine/src/main/java/Statistics.java
@@ -1,19 +1,62 @@
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.index.*;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.BytesRef;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 
+import static org.apache.lucene.index.MultiTerms.getTerms;
+
 public class Statistics {
+    
+
+    public static void basicInfo(String termText, String field) throws IOException {
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("index_folder")));
+
+        Term termInstance = new Term(field, termText);
+        long termFreq = reader.totalTermFreq(termInstance);
+        long docCount = reader.docFreq(termInstance);
+
+        System.out.println("term: "+termText+", termFreq = "+termFreq+", docCount = "+docCount);
+
+        reader.close();
+    }
+
+    public static void iterateThroughVocab(String field) throws IOException {
+
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("index_folder")));
+
+        double N = reader.numDocs();
+        double corpusLength = reader.getSumTotalTermFreq( field );
+
+        System.out.printf( "%-30s%-10s%-10s%-10s%-10s\n", "TERM", "DF", "TOTAL_TF", "IDF", "p(w|c)" );
+
+        // Get the vocabulary of the index.
+        Terms voc = getTerms( reader, field );
+        // You need to use TermsEnum to iterate each entry of the vocabulary.
+        TermsEnum termsEnum = voc.iterator();
+        BytesRef term;
+        int count = 0;
+        while ( ( term = termsEnum.next() ) != null ) {
+            count++;
+            String termstr = term.utf8ToString(); // get the text string of the term
+            int df = termsEnum.docFreq(); // get the document frequency (DF) of the term
+            long freq = termsEnum.totalTermFreq(); // get the total frequency of the term
+            double idf = Math.log( ( N + 1 ) / ( df + 1 ) );
+            double pwc = freq / corpusLength;
+            System.out.printf( "%-30s%-10d%-10d%-10.2f%-10.8f\n", termstr, df, freq, idf, pwc );
+            if ( count >= 100 ) {
+                break;
+            }
+        }
+
+        reader.close();
+    }
 
 
     public static void corpusLevelStatistics(String term, String field) throws IOException {
-        // Let's just count the IDF and P(w|corpus) for the word "reformulation" in the "text" field
 
         IndexReader index = DirectoryReader.open(FSDirectory.open(Paths.get("index_folder")));
-
 
         int N = index.numDocs(); // the total number of documents in the index
         int n = index.docFreq( new Term( field, term ) ); // get the document frequency of the term in the "text" field
@@ -32,9 +75,14 @@ public class Statistics {
     }
 
     public static void main(String[] args) throws IOException {
+
+        basicInfo("history", "content");
+
         corpusLevelStatistics("history", "abstract");
         corpusLevelStatistics("religion", "abstract");
         corpusLevelStatistics("science", "abstract");
+
+        iterateThroughVocab("content");
 
 
     }
